d2e957d87e993b6993fc0b1f90485a4175ce55e8
luzeqiang <luzeqiang@yuweitek.com>
2026-08-04 20:17:11 +0800

    添加定义消息ID接口

---
+++ src/define/MessageId.h
@@ -0,0 +1,15 @@
+/**
+ * @file MessageId.h
+ * @brief 消息ID定义，进程ipc标准协议
+*/
+
+#pragma once
+
+#include <cstdint>
+
+namespace IpcInterface {
+namespace Define {
+
+
+} // namespace Define
+} // namespace IpcInterface
